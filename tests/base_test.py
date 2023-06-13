a1d541395d6c996fc42974e35b46665c7c139c9d
azarouski <azarouski@solvd.com>
2023-06-13 12:19:51 +0300

    test reporting

--- tests/base_test.py
+++ tests/base_test.py
@@ -22,7 +22,7 @@ logger.setLevel(logging.INFO)
 # search_value = "Zebrunner"
 
 
-def test_simple(driver):
+def test_simple():
     # logger.info("'test_simple' test was started")
 
     # logger.info("Navigating to url: " + url)
